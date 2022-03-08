af734c66295eede3a773a678c7080db3ba346762
ManneredYouth <swepah.blast@gmail.com>
2022-03-08 20:22:33 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <table>
+      <tr>
+        <th>bruh</th>
+        <th>bruh2</th>
+      </tr>
+      <tr>
+        <td>bruh</td>
+        <td>bruh2</td>
+      </tr>
+    </table>
+  )
+}
+
+export default App;
